c4c1420c8809b538b5dc71e6be34f1c5e68eaed8
Pieter Overbeek <pieter@belfabriek.nl>
2024-08-17 08:02:04 +0300

    Partly implement CLI

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-    console.log('Server is running');
+export const serve = (post: number, filename: string, dir: string) => { 
+    console.log(`Serving on port ${post}`);
+    console.log(`Saving/fetching cells from ${filename}`);
+    console.log(`That file is in dir ${dir}`);
 }
